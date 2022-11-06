b2a9e87e986244bbe4c2d16c8a70e92f20806893
Zihann <marionqzh@gmail.com>
2022-11-06 17:22:52 +0800

    add url

--- .github/workflows/status-check.sh
+++ .github/workflows/status-check.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+if [ $1 == "cn" ] && [ $2 == "staging" ]
+  then url="https://stgapi.wooah.cn/api/Studio"
+elif [ $1 == "cn" ] && [ $2 == "production" ]
+  then url="https://api.wooah.cn/api/Studio"
+elif [ $1 == "global" ] && [ $2 == "staging" ]
+  then url="https://stgapi.d1.run/api/Studio"
+elif [ $1 == "global" ] && [ $2 == "production" ]
+  then url="https://api.d1.run/api/Studio"
+else echo "status=DOWN" >> $GITHUB_ENV; exit 1
+fi
+
+echo "$url"
 i=0
+
 flag=false
 while [ "$i" -lt 3 ]
 do
   sleep 1
-  CODE=`curl -I 'https://api.d1.run/api/Studio' 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+  CODE=`curl -I $url 2>/dev/null | head -n 1 | cut -d$' ' -f2`
   echo $CODE
   if [ $CODE == "200" ]; then
     flag=true
