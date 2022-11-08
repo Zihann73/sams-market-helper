bdbfe607f7fda7941e01be0b78282b776965fb16
Zihann <marionqzh@gmail.com>
2022-11-08 11:37:05 +0800

    add url

--- status-check.sh
+++ status-check.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-#if [ "$1" = "cn" ] && [ "$2" = "staging" ] && [ "$3" = "api" ]; then
-#  url="https://stgapi.wooah.cn/api/Studio"
-#elif [ "$1" = "cn" ] && [ "$2" = "production" ] && [ "$3" = "api" ]; then
-#  url="https://api.wooah.cn/api/Studio"
-#elif [ "$1" = "global" ] && [ "$2" = "staging" ] && [ "$3" = "api" ]; then
-#  url="https://stgapi.d1.run/api/Studio"
-#elif [ "$1" = "global" ] && [ "$2" = "production" ] && [ "$3" = "api" ]; then
-#  url="https://api.d1.run/api/Studio"
-#elif [ "$1" = "cn" ] && [ "$2" = "production" ] && [ "$3" = "document" ]; then
-#  url="https://api.wooah.cn/api/DocumentConverter/health"
-#elif [ "$1" = "global" ] && [ "$2" = "production" ] && [ "$3" = "document" ]; then
-#  url="https://api.d1.run/api/DocumentConverter/health"
-#else echo "status=DOWN" >> "$GITHUB_ENV"; exit 1
-#fi
-
 if [ "$1" = "global" ]; then
   region="d1.run"
 elif [ "$1" = "cnn" ]; then
   region="wooah.cn"
 else
   echo "status=DOWN" >> "$GITHUB_ENV"
-  echo "region env fail"
+  echo "The given region $1 does not exist."
   exit 1
 fi
 
@@ -29,20 +14,26 @@ if [ "$2" = "staging" ]; then
   stage="stgapi"
 elif [ "$2" = "production" ]; then
   stage="api"
-else echo "status=DOWN" >> "$GITHUB_ENV"; exit 1
+else
+  echo "status=DOWN" >> "$GITHUB_ENV"
+  echo "The given stage $2 does not exist."
+  exit 1
 fi
 
 if [ "$3" = "api" ]; then
   service="/api/Studio"
 elif [ "$3" = "document" ]; then
   service="/api/DocumentConverter/health"
-else echo "status=DOWN" >> "$GITHUB_ENV"; exit 1
+else
+  echo "status=DOWN" >> "$GITHUB_ENV"
+  echo "The given service $3 does not exist."
+  exit 1
 fi
 
 url="https://${stage}.${region}${service}"
 echo "$url"
-i=0
 
+i=0
 flag=false
 while [ "$i" -lt 30 ]
 do
